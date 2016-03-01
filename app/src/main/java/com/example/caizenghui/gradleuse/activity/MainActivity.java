2e8dff13121844103558cd0316081d6d1710fe4d
caizenghui <zenghui.cai@dianping.com>
2016-03-01 16:12:43 +0800

    update

---
+++ app/src/main/java/com/example/caizenghui/gradleuse/activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.caizenghui.gradleuse.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.caizenghui.barscanner.BarScannerActivity;
+import com.example.caizenghui.gradleuse.R;
+
+import utils.IntentUtils;
+
+public class MainActivity extends BaseMainActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_barscanner).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_barscanner:{
+                IntentUtils.startActivity(this, new Intent(this, BarScannerActivity.class));
+                break;
+            }
+        }
+    }
+}
---
+++ barscanner/src/main/java/com/example/caizenghui/barscanner/BarScannerActivity.java
@@ -0,0 +1,48 @@
+package com.example.caizenghui.barscanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class BarScannerActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_scanner);
+
+        findViewById(R.id.btn_back).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bar_scanner, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.btn_back) {
+            finish();
+        }
+    }
+}
