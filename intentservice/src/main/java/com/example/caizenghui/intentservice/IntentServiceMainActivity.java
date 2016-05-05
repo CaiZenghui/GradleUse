969ea81afb5bc275ff8f478499b8f9ac904c69da
caizenghui <zenghui.cai@dianping.com>
2016-05-05 15:37:46 +0800

    update IntentServiceMainActivity

--- intentservice/src/main/java/com/example/caizenghui/intentservice/IntentServiceMainActivity.java
+++ intentservice/src/main/java/com/example/caizenghui/intentservice/IntentServiceMainActivity.java
@@ -26,6 +26,9 @@ public class IntentServiceMainActivity extends Activity implements View.OnClickL
         }
         int i = v.getId();
         if (i == R.id.btn_start_service) {
+            // 下面这种形式连续 start 才会在同一个线程中执行；
+            // 正常第二次点击与第一次点击不会在同一个线程执行；
+            // 每一个 intent 处理完后会调用 stopSelf(msg.arg1)；调用该方法时若已有intent 待处理，则 msg.arg1 已更新，故service 不会被stop；否则会被stop；再有 intent 待处理则在新线程；
             startService(intent);
             startService(intent);
             startService(intent);
